bf8156dc73b83f1104a9ecb9013c0abc407cc4eb
pkch93 <star_final@naver.com>
2019-07-02 15:27:22 +0900

    feat: TodoForm styling

--- src/components/TodoBox/TodoForm.tsx
+++ src/components/TodoBox/TodoForm.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
-const TodoForm: React.FC = () => (
-    <div>
-        <input type="text" placeholder="할 일을 입력하세요!"/>
-        <button type="submit">Todo</button>
-    </div>
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Input = styled.input`
+    width: 75%;
+    height: 30px;
+    margin-left: 2%;
+`;
+
+const Button = styled.button<{ color: string }>`
+    background-color: ${({ color }) => color};
+    border: none;
+    width: 20%;
+    margin-right: 2%;
+    font-size: 20px;
+`;
+
+interface TodoFormProps {
+    color: string;
+}
+
+const TodoForm: React.FC<TodoFormProps> = ({ color }) => (
+    <Wrapper>
+        <Input type="text" placeholder="할 일을 입력하세요!"/>
+        <Button color={color} type="submit">Todo</Button>
+    </Wrapper>
 );
 
 export default TodoForm;
--- src/test/TodoBox/TodoForm.test.tsx
+++ src/test/TodoBox/TodoForm.test.tsx
@@ -5,12 +5,12 @@ import TodoForm from '../../components/TodoBox/TodoForm';
 
 describe('<TodoForm />', () => {
     it("matches snapshot", () => {
-        const utils = render(<TodoForm />);
+        const utils = render(<TodoForm color={'#66d9e8'} />);
         expect(utils.container).toMatchSnapshot();
     });
 
     it('have one submit button and one input box', () => {
-        const target = render(<TodoForm />);
+        const target = render(<TodoForm color={'#66d9e8'} />);
         target.getByPlaceholderText("할 일을 입력하세요!");
         target.getByText("Todo");
     });
